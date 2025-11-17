935e3f1ab71f946910f6a0aa882ad168cc11371e
Eduardo Jara <eduardojara01@gmail.com>
2025-11-17 18:41:24 -0300

    Agregar favicon SVG y eliminar error 404

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($pageTitle) ? $pageTitle . ' - ' : ''; ?><?php echo APP_NAME; ?></title>
+    <link rel="icon" type="image/svg+xml" href="<?php echo APP_URL; ?>/assets/favicon.svg">
     <link rel="stylesheet" href="<?php echo APP_URL; ?>/assets/css/style.css">
 </head>
 <body>
