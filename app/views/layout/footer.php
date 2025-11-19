9d4740b68865c5dad8171470556ca1a426d2a7c7
Eduardo Jara <eduardojara01@gmail.com>
2025-11-19 19:05:36 -0300

    agregar versión al archivo JS para forzar recarga del cache del navegador

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -6,6 +6,6 @@
         <p style="font-size: 0.9rem; margin-top: 0.5rem;">Versión <?php echo APP_VERSION; ?></p>
     </footer>
     
-    <script src="<?php echo APP_URL; ?>/assets/js/main.js"></script>
+    <script src="<?php echo APP_URL; ?>/assets/js/main.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
